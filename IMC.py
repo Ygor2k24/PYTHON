98b3fe4d7770e617d7ddc37e22e2212d1973ce1e
Ygor2k24 <ygorrodriguesdasilva08@gmail.com>
2024-12-02 20:21:27 -0300

    python

--- IMC.py
+++ IMC.py
@@ -6,8 +6,13 @@ print(f"Olá {nome}. Aguarde alguns instantes, até calcularmos o seu Indice de 
 
 if peso / (altura ** 2):
 
+imc = 0
+
 print(f"Seu IMC é {imc:.2f}.")
 
 if imc < 18.5:
     print(f"Você está abaixo do peso.")
-if 18.5 <= imc <
+if 18.5 <= imc < 29.99:
+    print(f"Você está com o peso normal.")
+if 30.0:
+    pass
